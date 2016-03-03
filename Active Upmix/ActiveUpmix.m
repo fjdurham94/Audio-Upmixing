d3bc635c66a8f7b9997728590ed6dac661575b6e
fjdurham94 <jackdurham@jacks-mbp.home>
2016-03-03 13:01:55 +0000

    "Plots log l - log r for each sample of the first frame."

--- Active Upmix/ActiveUpmix.m
+++ Active Upmix/ActiveUpmix.m
@@ -9,18 +9,23 @@ function ActiveUpmix(inputfile)
     % Find the dominance vector
     start_frame = 1;
     frame = input(start_frame*F_SIZE : start_frame*F_SIZE+F_SIZE,:);
+    
+    % psv_Matrix = PassiveMatrix(frame(:,1), frame(:,2), Fs);
+    
+    % Find the dominance vector
     frame = log(abs(frame)); % log of full wave rectified sample values
     
     %biplot(frame)
     %clf;
-    scatter(frame(:,1), frame(:,2), '.g');
-    hold on; xlabel('L'); ylabel('R');
+    plot(frame(:,1) - frame(:,2), '.g');
+    hold on; 
+    xlabel('n'); ylabel('log l - log r');
     scatter(mean(frame(:,1)), mean(frame(:,2)), 'xm');
     % Adjust axis
     ax = gca;
     axis equal;
     ax.XAxisLocation = 'origin'; ax.YAxisLocation = 'origin';
-    ax.XLim = [-10 10]; ax.YLim = [-10 10];
+    %ax.XLim = [-10 10]; ax.YLim = [-10 10];
     
     % Dominance bipolar control signal
     Elr = frame(:,1) - frame(:,2);
