da187936a24bdc72d23f6925af78e61bf865b5d7
fjdurham94 <jackdurham@Jacks-MacBook-Pro.local>
2016-03-07 13:48:56 +0000

    "Filters and phase delay moved to another function."

--- PassiveMatrix.m
+++ PassiveMatrix.m
@@ -5,19 +5,17 @@ function output = PassiveMatrix(left, right, Fs)
     % Surround channel needs to be phase deleayed and
     % filtered at 7khz after this function to allow the active upmixer to
     % perform dominance detection.
+    fprintf('Applying passive matrix\n');
     
     c=(left+right)/sqrt(2);
 
-    % Low pass for the LFE at 120Hz ITU-R 775
-    fprintf('Filtering LFE above 120Hz\n');
-    lpfspec120Hz = fdesign.lowpass('Fp,Fst,Ap,Ast',120,250,0.1,50,Fs);
-    lpf120Hz = design(lpfspec120Hz, 'equiripple');
-    LFE=filter(lpf120Hz, c);
+    % Take the LFE as the centre channel for now, LPF to be applied after
+    % this function.
+    LFE = c;
 
     rl=(left-right)/sqrt(2); %3dB attenuation to maintain constant acoustic energy
 
     rr = -rl; % Take the antiphase of the difference signal, same as -90 phase shift of r-l
-
     
     % Bring together for a 6 channel output
     output = [left, right, c, LFE, rl, rr];
